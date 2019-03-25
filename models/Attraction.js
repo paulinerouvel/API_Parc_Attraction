660e5076f231e8518cc134f5927da7c01a6b517f
pauline.rouvel <pauline.rouvel@hotmail.fr>
2019-03-25 22:32:31 +0100

    add object attraction and pass, begining of the controllers and functions

---
+++ models/Attraction.js
@@ -0,0 +1,24 @@
+'use strict'
+
+class Attraction{
+    constructor(name, description, type, capacity, duration, openingHours, handicapAccess, withAdulteAccess, maintenance, images){
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.capacity = capacity;
+        this.duration = duration;
+        this.openingHours = openingHours;
+        this.handicapAccess = handicapAccess;
+        this.withAdulteAccess = withAdulteAccess;
+        this.maintenance = maintenance;
+        this.images = images;
+    }
+
+    //update information
+    //add images
+    //delete images
+
+
+}
+
+module.exports = Attraction;
